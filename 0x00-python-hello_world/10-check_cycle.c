8f95bbade6d9d4ee92087d0b484f37d106ba0aa3
root <smsmelshamey90@gmail.com>
2023-11-27 14:10:26 -0800

    list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_cycle - check if list is cycle
+ *
+ * @list: list
+ * Return: integer
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1 = list, *ptr2 = list;
+
+	while (ptr2 && ptr2->next)
+	{
+		ptr1 = ptr2->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
