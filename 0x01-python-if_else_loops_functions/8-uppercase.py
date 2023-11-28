5bb9b54bc84f6a9e9b26f8319e928df40fc4d5b4
root <smsmelshamey90@gmail.com>
2023-11-28 11:50:30 -0800

    list cycle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for ch in str:
         if ord('z') >= ord(ch) >= ord('a'):
-           ch = chr(ord(ch) - 32)
+            ch = chr(ord(ch)-32)
         print("{:s}".format(ch), end="")
     print()
