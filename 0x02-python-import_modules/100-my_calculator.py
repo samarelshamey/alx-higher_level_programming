cf2c09282b89799b2f7c9db047633f150f026286
root <smsmelshamey90@gmail.com>
2023-11-30 12:35:30 -0800

    loops and functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,16 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+    
     argc = len(sys.argv)
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-    if argc != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+
     if op == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif op == "-":
@@ -19,4 +22,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        exit (1)
