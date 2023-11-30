add7711879909ec3598e344ab083d2a00770b5e8
root <smsmelshamey90@gmail.com>
2023-11-30 12:39:25 -0800

    loops and functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
+
     argc = len(sys.argv)
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
