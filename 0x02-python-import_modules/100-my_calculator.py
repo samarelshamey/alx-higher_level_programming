785433769dd12b1435f0c77825bc03cd759786ec
root <smsmelshamey90@gmail.com>
2023-11-30 12:33:22 -0800

    loops and functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,21 +4,19 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     argc = len(sys.argv)
     a = int(sys.argv[1])
-    op = int(sys.argv[2])
+    op = sys.argv[2]
     b = int(sys.argv[3])
     if argc != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    for op in range("+", "-", "*", "/"):
-        if op == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif op == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        
+    if op == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
