af9c435a7023039a5254f5845a4f5be21744dcef
root <smsmelshamey90@gmail.com>
2023-12-04 13:10:17 -0800

    list and tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2]
-    b1, b2 = tuple_b[:2]
-    if len(tuple_a) < 2:
-        a2 = 0
-    if len(tuple_b) < 2:
-        b2 = 0
-    c1 = a1 + b1
-    c2 = a2 + b2
-    return (c1, c2)
+    len_a, len_b = len(tuple_a), len(tuple_b)
+    tuple_c = ((tuple_a[0] if len_a >= 1 else 0) +
+            (tuple_b[0] if len_b >= 1 else 0),
+            (tuple_a[1] if len_a >= 2 else 0) +
+            (tuple_b[1] if len_b >= 2 else 0))
+    return tuple_c
