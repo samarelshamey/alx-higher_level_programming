ccfdbdf96593d8eb7cba20fb0eaedcb89bc64feb
root <smsmelshamey90@gmail.com>
2023-12-04 12:58:59 -0800

    list and tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1, a2 = tuple_a[:2]
+    b1, b2 = tuple_b[:2]
+    if len(tuple_a) < 2:
+        a2 = 0
+    if len(tuple_b) < 2:
+        b2 = 0
