0e76b89cb8d20fe98e8f5a4f29c21faff57f3977
root <smsmelshamey90@gmail.com>
2023-12-04 14:29:14 -0800

    list and tuple

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,7 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         my_list == []
+    else:
         for i in my_list:
             my_list.append(False if i % 2 else True)
         return my_list
