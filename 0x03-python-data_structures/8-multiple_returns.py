842124b5704cdb01207647ce23c7b9cee310ef31
root <smsmelshamey90@gmail.com>
2023-12-04 13:59:07 -0800

    list and tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if sentence is None:
         sentence[0] = None
         return sentence[0]
-    return (len(sentence), sentence[0])
+    else:
+        return (len(sentence), sentence[0])
