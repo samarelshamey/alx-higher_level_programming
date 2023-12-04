e89641b10358beca50c12721aa61496b58dbb3c8
root <smsmelshamey90@gmail.com>
2023-12-04 11:10:34 -0800

    list and tuple

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for line in matrix:
         for n in line:
             print("{:d}".format(n), end=" ")
