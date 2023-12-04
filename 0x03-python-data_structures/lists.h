a319733275e2e1d77529de850aaf595f80b085ad
root <smsmelshamey90@gmail.com>
2023-12-04 15:45:14 -0800

    list and tuple

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -14,7 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
+int palin(listint_t **head, listint_t *end);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
