5b1364eff998c3f8271b2aca07cbc6618a414c9f
root <smsmelshamey90@gmail.com>
2023-12-04 16:10:00 -0800

    list and tuple

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,18 +1,27 @@
-#include <stdio.h>
-#include <python.h>
-#include <stdlib.h>
+#include <Python.h>
+
 /**
- * print_python_list_info - printing python info
+ * print_python_list_info - print list info
  *
  * @p: python list
+ *
  * Return: nothing
 */
 void print_python_list_info(PyObject *p)
 {
-	int element;
+	int allocate, i, size;
+	PyObject *obj;
 
-	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (elemet = 0; element <  Py_SIZE(p); element++)
-		printf("Element %d: %s\n", element, Py_TYPE(PyList_GetItem(p, element))->tp_name);
+	size = Py_SIZE(p);
+	allocate = ((PyListObject *)p)->allocated;
+	printtf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocate);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
 }
