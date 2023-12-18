7145c2c013706ebd68df505f9cc58519d5adf65b
root <smsmelshamey90@gmail.com>
2023-12-18 09:07:31 -0800

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end=" ")
             count += 1
-    except IndexError:
+    except:
         pass
-    finally:
-        print()
     return count
