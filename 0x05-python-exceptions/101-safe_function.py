a8f6d5f8e4ae17b09c12c73ef2d04b6eae643c54
root <smsmelshamey90@gmail.com>
2023-12-18 10:19:17 -0800

    exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        i = fct(*args)
+        return i
+    except Exception as err:
+        print("Exception: {}".format(err), file=stderr)
+        return None
