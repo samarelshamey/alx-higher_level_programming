7a2e957ddae2b78be1e12eb207244b3cd2c4ad49
root <smsmelshamey90@gmail.com>
2023-12-19 10:01:55 -0800

    oop

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for suqare"""
+
+
+class Square:
+    """define a square"""
+    def _init_(self, size):
+        """Construct/
+
+        Args:
+            size: square side length
+        """
+        self.__size = size
