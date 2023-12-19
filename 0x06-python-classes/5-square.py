eb240d69437e28f8021cd95be2e6602fd4eb2970
root <smsmelshamey90@gmail.com>
2023-12-19 10:44:15 -0800

    exceptions

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+Square = __import__('4-square').Square
+
+
+def my_print(self):
+    """print a square"""
+    for i in range(self.size):
+        for j in range(self.size):
+            if self.size = 0:
+                print()
+            else:
+                print("#", end="\n")
