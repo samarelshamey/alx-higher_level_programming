0f820ad36cf7cf9e578e677c46133a9aa0e44d77
root <smsmelshamey90@gmail.com>
2024-01-04 10:03:50 -0800

    more classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """define a rectangle"""
 
     number_of_instances = 0
+    print_symbo = '#'
 
     def __init__(self, width=0, height=0):
         """Construct/
@@ -68,17 +69,32 @@ class Rectangle:
 
     def __str__(self):
         """return string representative of a rectangle"""
-        rectangle_str = ""
-        if self.__width != 0 and self.__height != 0:
-            rectangle_str += "\n".join("#" * self.width
-                                       for _ in range(self.height))
-        return rectangle_str
+        if not self.__width or not self.__height:
+            return ""
+        return ((str(self.print_symbol) * self.width + "\n") *
+                self.height)[:-1]
 
     def __repr__(self):
         """return string rep of rectangle for production"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
 
     def __del__(self):
         """print a message for deletion"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    def bigger_or_equal(rect_1, rect_2):
+        """return the bigger rectangle"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """return new Rectangle instance with width == height == size"""
+        return cls(size, size)
