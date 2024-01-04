ee872b2c5d94237a1f5ca6c2ab9433d31141e5a2
root <smsmelshamey90@gmail.com>
2024-01-04 07:17:18 -0800

    more classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -64,9 +64,9 @@ class Rectangle:
 
     def __str__(self):
         """return string representative of a rectangle"""
+        rectangle_str = ""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
         return rectangle_str
