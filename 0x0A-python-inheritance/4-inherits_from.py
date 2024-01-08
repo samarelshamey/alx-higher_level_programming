6262d330c8aa3c5825f9678dcc8918d3049349dd
root <smsmelshamey90@gmail.com>
2024-01-08 08:21:16 -0800

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """check if the object is an instance of a class that inherited
+
+    Args:
+        obj: object
+        a_class: class
+
+    Return:
+        true or false
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
