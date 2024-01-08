2d05c3f88562b396803fa99e478f6fd8eeeb0631
root <smsmelshamey90@gmail.com>
2024-01-08 08:27:44 -0800

    inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module for BaseGeometry."""
+
+
+class BaseGeometry:
+    """class for basegeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
