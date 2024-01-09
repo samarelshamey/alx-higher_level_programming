2adc29a3068a7d301df59b7101a3bd97a8c325df
root <smsmelshamey90@gmail.com>
2024-01-09 06:07:59 -0800

    input and output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,20 @@
 
 
 import sys
+from os.path import exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-argslst = list(sys.argv[1:])
 
-try:
-    data = load_from_json_file('add_item.json')
-except Exception:
-    data = []
+def add_items_to_list(items):
+    if exists("add_item.json"):
+        existing_data = load_from_json_file('add_item.json')
+        existing_data.extend(items)
+        save_to_json_file(existing_data, "add_item.json")
+    else:
+        new_data = items
+        save_to_json_file(new_data, "add_item.json")
 
-data.extend(argslst)
-save_to_json_file(data, 'add_item.json')
+
+argslst = sys.argv[1:]
+add_items_to_list(argslst)
