58cd9e9020fbc0dc9f02a7e03e394f04d9d32757
root <smsmelshamey90@gmail.com>
2024-01-09 05:57:37 -0800

    input and output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for all args"""
+
+
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+argslst = list(sys.argv[1:])
+
+try:
+    data = load_from_json_file('add_item.json')
+except Exception:
+    data = []
+
+data.extend(argslst)
+save_to_json_file(data, 'add_item.json')
