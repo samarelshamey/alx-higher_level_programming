b31949ee804f130da90c31661540e5cf582dcdbf
root <smsmelshamey90@gmail.com>
2024-01-09 05:25:32 -0800

    input and output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """appends a string at the end of a text file"""
-    with open(filename, 'a', encoding='utf-8') as file_append:
-        return file_append.append(text)
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for to_json_string"""
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object (string)"""
+    with open
