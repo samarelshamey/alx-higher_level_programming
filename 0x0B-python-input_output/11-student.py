ef55b46c7dff3213925df60d25a503bc2efa92ce
root <smsmelshamey90@gmail.com>
2024-01-09 14:18:14 -0800

    input and output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module for class student"""
+
+
+class Student:
+    """class for student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        if not isinstance(attrs, list):
+            return self.__dict__
+        diction = dict()
+        for attr in attrs:
+            if not isinstance(attr, str):
+                return self.__dict__
+            if attr in self.__dict__:
+                diction[attr] = self.__dict__[attr]
+        return diction
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
