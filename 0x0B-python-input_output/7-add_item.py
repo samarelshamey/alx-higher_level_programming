f0236f00ddb8cb44608ae73e1345f29cc08a1be7
root <smsmelshamey90@gmail.com>
2024-01-09 05:58:50 -0800

    input and output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """module for all args"""
 
 
-import json
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
