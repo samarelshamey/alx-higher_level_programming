b346a01a03d4a5ad773531bbf1b95f0ec358d384
root <smsmelshamey90@gmail.com>
2024-01-15 09:07:43 -0800

    almost circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""module for a rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """define a rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        print("\n" * self.y, end="")
+        for _ in range(self.__height):
+            print(" " * self.x, end="")
+            print("#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".\
+                format(self.id, self.x, self.y, self.width, self.height)
+
+    def update_args(self, id=None, width=None, height=None, x=None, y=None):
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        if args:
+            self.update_args(*args)
+        elif kwargs:
+            self.update_args(**kwargs)
+
+    def to_dictionary(self):
+        return {"x": self.__x, "y": self.__y, "id": self.id,
+                "height": self.__height, "width": self.__width}
