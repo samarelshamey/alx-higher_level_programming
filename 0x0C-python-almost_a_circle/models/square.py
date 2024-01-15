2cbac30752861eb17439c01aed342a07dd540890
root <smsmelshamey90@gmail.com>
2024-01-15 14:54:58 -0800

    almost circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 '''module for square class'''
-Rectangle = __import__('rectangle').Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """define a square"""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """initiate square"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """size of square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
     def __str__(self):
+        """return  str representation of square"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".\
                 format(self.id, self.x, self.y, self.width)
 
     def update_args(self, id=None, size=None, x=None, y=None):
+        """update arguments"""
         if id is not None:
             self.id = id
         if size is not None:
@@ -36,11 +37,13 @@ class Square(Rectangle):
             self.y = y
 
     def update(self, *args, **kwargs):
+        """update square"""
         if args:
             self.update_args(*args)
         elif kwargs:
             self.update_args(**kwargs)
 
     def to_dictionary(self):
+        """return dictionary"""
         return {"id": self.id, "x": self.x,
                 "size": self.width, "y": self.y}
