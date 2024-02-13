a991df835180ec117e27b4899c887f0391e91c30
root <smsmelshamey90@gmail.com>
2024-02-13 11:00:47 -0800

    SQL intro

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Script creates a table called first_table
 -- id INT
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
