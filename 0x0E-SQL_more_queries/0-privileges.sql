51f1278240eba1ba1eec1e4647c311edd6d40365
root <smsmelshamey90@gmail.com>
2024-02-14 10:36:22 -0800

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SELECT User, Host, Grant_priv, Create_priv, Insert_priv, Update_priv, Delete_priv, Select_priv
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
