2ea89c665009523415a1e2eb7da826c88e00a7ad
root <smsmelshamey90@gmail.com>
2024-02-14 12:27:12 -0800

    sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Scipt that lists all Comedy shows in the database 
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
