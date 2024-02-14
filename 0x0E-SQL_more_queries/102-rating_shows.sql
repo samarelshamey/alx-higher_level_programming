0027f825338cee366a0063daaedde6f2471a8aac
root <smsmelshamey90@gmail.com>
2024-02-14 12:50:39 -0800

    sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT title, SUM(tv_show_rating.rate) 'rating'
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-LEFT JOIN tv_show_rating ON tv_shows.id = tv_show_rating.show_id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
 ORDER BY rating DESC;
