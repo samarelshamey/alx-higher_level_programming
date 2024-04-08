51f9a56fc53d5fa84eb2e46793ed74bfc0c65f65
root <smsmelshamey90@gmail.com>
2024-04-08 08:39:13 -0700

    js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const x = parseInt(arg);
+if (Number.isNaN(Number(arg))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let row = '';
+    for (let j = 0; j < x; j++) {
+      row += 'X';
+    }
+      console.log(row);
+}
+}
