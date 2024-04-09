d1e146f2c7d0a921cc2180d363537fb301b69c71
root <smsmelshamey90@gmail.com>
2024-04-09 12:44:53 -0700

    js classes

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,14 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-
+  let length = list.length - 1;
+  let i = 0;
+  while ((length - i) > 0) {
+    const x = list[length];
+    list[length] = list[i];
+    list[i] = x;
+    i++;
+    length--;
+  }
+  return list;
+};
 }
