ea41096f39d0101e0aec585f28edccb93bc66e83
root <smsmelshamey90@gmail.com>
2024-04-11 15:04:50 -0700

    Object Realational Mangment

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+"""module that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+    mydb = MySQLdb.connect(
+            host="localhost",\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    myresult = cursor.fetchall()
+
+    for x in myresult:
+        print(x)
+    mycursor.close()
+    mydb.close()
