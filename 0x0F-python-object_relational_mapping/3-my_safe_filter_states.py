ee09ed6d10e2cb3a646bc3a5e56cc03e76e06765
root <smsmelshamey90@gmail.com>
2024-04-12 10:22:10 -0700

    Object Realational Mangment

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,29 +5,20 @@ and displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument
 """
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    """access to database"""
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
 
-    try:
-        mydb = MySQLdb.connect(hotsname="localhost",
-                               port=3306,
-                               user=username,
-                               passwd=password,
-                               db=database)
-    except MySQLdb.Error as e:
-        print("Error connecting to database: {}".format(e))
-        sys.exit(1)
-
-        mycursor = mydb.cursor()
-        mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY %(state_name)s
-                         ORDER BY id ASC", {'state_name: sys.argv[4]'})
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            print(x)
-        mycursor.close()
-        mydb.close()
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY % (name)s
+                     ORDER BY id ASC", {'name': argv[4]})
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
