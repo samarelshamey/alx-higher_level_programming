6ad88330b1b8b21066911309c7eccdfc2337cd71
root <smsmelshamey90@gmail.com>
2024-04-12 10:27:04 -0700

    Object Realational Mangment

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
                            db=argv[3])
 
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY % (name)s
-                     ORDER BY id ASC", {'name': argv[4]})
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    mycursor.execute(query, (state_name,))
     myresult = mycursor.fetchall()
     for x in myresult:
         print(x)
