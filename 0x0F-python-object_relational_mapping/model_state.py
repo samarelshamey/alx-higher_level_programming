ff9f09a941edc59c728868f3108845a7f5fabd34
root <smsmelshamey90@gmail.com>
2024-04-12 16:29:43 -0700

    Object Realational Mangment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,8 +4,15 @@ from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
+
+
 class State(Base):
-    """state class"""
+    """state class
+    Attributes:
+        __tablename__ (str): table name
+        id (int): state id
+        name (str): state name of class
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128))
+    name = Column(String(128), nullable=False)
