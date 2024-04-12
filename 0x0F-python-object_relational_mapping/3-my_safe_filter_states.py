6b309325b26f6893b39dd62adfc060fc0220ab7a
root <smsmelshamey90@gmail.com>
2024-04-12 10:05:11 -0700

    Object Realational Mangment

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+module that takes in arguments
+and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        mydb = MySQLdb.connect(hotsname="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=password,
+                               db=database)
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'
+                         ORDER BY id ASC".format(state_name))
+        myresult = mycursor.fetchall()
+        for x in myresult:
+            print(x)
+        mycursor.close()
+        mydb.close()
