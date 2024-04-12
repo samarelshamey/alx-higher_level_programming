fe7737e5be2e1dcc561128783194c9d908ec4fff
root <smsmelshamey90@gmail.com>
2024-04-12 15:35:35 -0700

    Object Realational Mangment

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """
-module that takes in arguments
-and displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument
+module that takes name of a state as an argument and lists all cities
 """
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ == '__main__':
-    """access to database"""
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
+
     try:
         mydb = MySQLdb.connect(host="localhost",
                                port=3306,
@@ -26,7 +23,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     mycursor = mydb.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    query = "SELECT * FROM cities WHERE name = %s ORDER BY cities.id ASC"
     mycursor.execute(query, (state_name,))
     myresult = mycursor.fetchall()
     for x in myresult:
