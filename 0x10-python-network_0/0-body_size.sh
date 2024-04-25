f4c773f5fb7eefba3f6098b031979050029dbdbd
root <smsmelshamey90@gmail.com>
2024-04-25 09:50:18 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,10 @@
 # Bash script that takes in a URL,
 # sends a request to that URL,
 # and displays the size of the body of the response
-curl -s "${1}" | wc -c
+url=$1
+body_size=$(curl -sI $url | grep -i Content-Length | awk '{print $2}')
+if [ -z "$body_size" ]; then
+    echo "Error: Couldn't fetch content size"
+else
+    echo $body_size
+fi
