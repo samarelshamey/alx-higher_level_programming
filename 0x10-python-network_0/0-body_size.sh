8fba6940050350056ff5d661c58dd502e8708df6
root <smsmelshamey90@gmail.com>
2024-04-25 09:15:46 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,5 @@
 # Bash script that takes in a URL,
 # sends a request to that URL,
 # and displays the size of the body of the response
-reponse="curl -sI "$1" | grep -i Content-Length"
+response=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}')
 echo "$response"
