d8533f18e9c917ef198208d2cb6d2f14924bf814
root <smsmelshamey90@gmail.com>
2024-04-26 14:26:44 -0700

    requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,13 +4,13 @@ script that takes your GitHub credentials
 uses the GitHub API to display your id
 """
 if __name__ == "__main__":
-    import requests
+    from requests import get
     import sys
 
     username = sys.argv[1]
     password = sys.argv[2]
     url = 'https://api.github.com/user'
-    req = requests.get(url, auth=(username, password))
+    req = get(url, auth=(username, password))
     if req.status_code == 200:
         user_info = req.json()
         user_id = user_info.get('id')
