debbb92957b319005b59cbd5e9da90956e43904f
root <smsmelshamey90@gmail.com>
2024-04-26 11:38:06 -0700

    urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Write a Python script that fetches url
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+print('Body response:')
+print('\t- type: ', type(html))
+print('\t- content: ', html)
+print('\t- utf8 content: ', html.decode('utf-8'))
