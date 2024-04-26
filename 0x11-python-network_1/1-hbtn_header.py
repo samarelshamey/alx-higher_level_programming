bd10cbefe2cee891039ad5c63758491e1f31f445
root <smsmelshamey90@gmail.com>
2024-04-26 12:00:00 -0700

    urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,4 +8,4 @@ url = sys.argv[1]
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as response:
     X_Request_Id = response.getheader('X-Request-Id')
-    print(X_Request_Id)
+print(X_Request_Id)
