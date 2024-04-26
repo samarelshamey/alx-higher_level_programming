cfb43c8c759ef0a93d361aa663ea02caa2c176b2
root <smsmelshamey90@gmail.com>
2024-04-26 11:58:32 -0700

    urllib

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Python script that takes in a URL, sends a request to the URL
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    X_Request_Id = response.getheader('X-Request-Id')
+    print(X_Request_Id)
