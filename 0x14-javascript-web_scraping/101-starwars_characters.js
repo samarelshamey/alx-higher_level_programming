56206775b49ad67682ad4f7c781583d790b74a3a
root <smsmelshamey90@gmail.com>
2024-05-21 12:32:38 -0700

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request.get(url, function (error, response, body) {
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     const content = JSON.parse(body);
     const characters = content.characters;
-    characters.forEach(characterUrl => {
-      request.get(characterUrl, function (error, response, body) {
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
         if (error) {
-          console.error(error);
+          console.log(error);
         } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
+          const names = JSON.parse(body);
+          console.log(names.name);
         }
       });
-    });
+    }
   }
 });
